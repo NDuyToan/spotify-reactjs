8daacba87359491ce3f642edb85501ed9f95e237
Toan Nguyen <toan.nguyen@smartdev.com>
2022-06-28 23:02:20 +0700

    add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Navbar(props) {
+    return (
+        <div className='h-24 bg-slate-900 text-center text-white leading-[6rem] text-3xl'>
+            <i className='fa fa-spotify mr-5'></i>
+            Spotify
+        </div>
+    );
+}
+
+export default Navbar;
