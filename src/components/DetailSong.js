7852b238764db05b1a3d34b3c6fba578fe5e4ba6
ToanNguyen <toannguyen@SmartDevs-Mac-mini.local>
2022-06-29 10:49:04 +0700

    detail song

--- src/components/DetailSong.js
+++ src/components/DetailSong.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
 
 function DetailSong(props) {
-    return (
-        <div className='col-span-1'>
-            detail
+  return (
+    <div className="col-span-1 p-3">
+      <h2 className="text-sky-400 font-bold">Now playing</h2>
+      <h2 className="text-neutral-400 text-2xl">Sing me to sleep</h2>
+      <div className="w-[256px] m-auto mt-20">
+        <img
+          className="w-full"
+          src="https://i.scdn.co/image/ab6761610000e5ebc02d416c309a68b04dc94576"
+          alt="avatar"
+        />
+        <div className="flex justify-between items-center mt-5">
+          <img
+            className="w-[70px] rounded-full"
+            src="https://i.scdn.co/image/ab67616d0000b273a108e07c661f9fc54de9c43a"
+            alt="singer"
+          />
+          <span className="text-xl text-white">Alan Walker</span>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default DetailSong;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
 function Navbar(props) {
-    return (
-        <div className='h-24 bg-slate-900 text-center text-white leading-[6rem] text-3xl'>
-            <i className='fa fa-spotify mr-5'></i>
-            Spotify
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-center h-[100px] bg-gray-900 text-center text-white text-[35px] ">
+      <i className="fa fa-spotify mr-5"></i>
+      Spotify
+    </div>
+  );
 }
 
 export default Navbar;
